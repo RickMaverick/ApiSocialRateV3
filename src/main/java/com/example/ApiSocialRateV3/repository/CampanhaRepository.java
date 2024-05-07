b21d69820f81a903156869f8229df968942ca9d9
Ricardo Temple <122487111+RickMaverick@users.noreply.github.com>
2024-05-06 22:55:50 -0300

    Cria CampanhaRepository

---
+++ src/main/java/com/example/ApiSocialRateV3/repository/CampanhaRepository.java
@@ -0,0 +1,7 @@
+package com.example.ApiSocialRateV3.repository;
+
+import com.example.ApiSocialRateV3.model.Campanha;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CampanhaRepository extends JpaRepository<Campanha, Long> {
+}
